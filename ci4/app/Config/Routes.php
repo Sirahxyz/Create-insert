bb25c71eb83566c830257fd2bc60e91993eccee3
Sirahxyz <harisafifudin123@gmail.com>
2024-05-19 10:31:03 +0700

    Commit pertama

---
+++ ci4/app/Config/Routes.php
@@ -0,0 +1,13 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Page::index');
+$routes->get('/books/index', 'Books::index');
+$routes->get('/books/create', 'Books::create');
+$routes->get('/books/save', 'Books::save');
+$routes->delete('/books/(:num)', 'Books::delete/$1');
+$routes->get('/books/(:any)', 'Books::detail/$1');
